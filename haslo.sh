3b4542a99dfc7c1d908a86557030b498d08136b5
julia <148009758+P4R4D1GMSH1FT@users.noreply.github.com>
2024-04-23 11:21:55 +0200

    Update

---
+++ haslo.sh
@@ -0,0 +1,11 @@
+$ echo -n "Podaj haslo: "; read -s haslo; echo;
+echo "Podane haslo: $haslo"
+Podaj haslo:
+Podane haslo:
+
+# wersja z czasem 10 sekund
+
+$ echo -n "Podaj haslo: "; read -t 10 -s haslo; echo;
+echo "Podane haslo: $haslo"
+Podaj haslo:
+Podane haslo:
