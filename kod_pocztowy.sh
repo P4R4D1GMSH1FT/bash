13e19d96674bbf4ad3d9dbbba62464b508bfeb2c
julia <148009758+P4R4D1GMSH1FT@users.noreply.github.com>
2024-04-23 11:36:44 +0200

    Update kod_pocztowy.sh

--- kod_pocztowy.sh
+++ kod_pocztowy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#odczytanie kodu w formacie dd-ddd
 echo "Podaj kod pocztowy: "; read kod_pocztowy
 
+#uzycie zmiennej IFS i przypisanie jej do myslnika
 IFS='-'
 
+#zmiana zapisu na bez myslnika
 kod_bez_myslnika=""
 for fragment in $kod_pocztowy; do kod_bez_myslnika="${kod_bez_myslnika}${fragment}"
 done
