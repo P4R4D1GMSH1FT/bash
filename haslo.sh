1904421205654ffad2a36b15531d9b99f56994bd
julia <148009758+P4R4D1GMSH1FT@users.noreply.github.com>
2024-04-28 20:58:23 +0200

    Update haslo.sh

--- haslo.sh
+++ haslo.sh
@@ -3,13 +3,13 @@
 #wersja z stdout
 
 $ echo -n "Podaj haslo: "; read -s haslo; echo;
-echo "Podane haslo: $haslo"
+  echo "Podane haslo: $haslo"
 Podaj haslo:
 Podane haslo:
 
 #wersja z czasem 10 sekund
 
 $ echo -n "Podaj haslo: "; read -t 10 -s haslo; echo;
-echo "Podane haslo: $haslo"
+  echo "Podane haslo: $haslo"
 Podaj haslo:
 Podane haslo:
